5bcad98046026c569f4ec389604ad08d26d4cb4c
Talha Bayansar <talhabayansar@be-c02zx2b3md6m.ordina.belgium>
2023-04-25 13:19:31 +0200

    start of chapter 1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,21 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#ebf8ff",
+          200: "#bee3f8",
+          300: "#E37C30",
+          400: "#63b3ed",
+          500: "#E7904B",
+          600: "#3182ce",
+          700: "#EAB285",
+          800: "#2c5282",
+          900: "#F7D1A7",
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
